4248dc16483b85a9600aefc345c724177bdcd654
ramses <ramsescab@github.com>
2026-01-08 11:49:39 +0100

    first commit

---
+++ src/books.js
@@ -0,0 +1,29 @@
+const books = [
+  {
+    title: "Title 1",
+    author: "Author 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    title: "Title 2",
+    author: "Author 2",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    title: "Title 3",
+    author: "Author 3",
+    description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  },
+  {
+    title: "Title 4",
+    author: "Author 4",
+    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    title: "Title 5",
+    author: "Author 5",
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam."
+  }
+]
+
+export default books
